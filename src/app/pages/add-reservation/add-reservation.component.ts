f6a8e95079e4f9a0daf2170a9ab46d56df1c3453
Ayoub Brahim Hennani <hennani.2020@gmail.com>
2024-05-06 16:15:46 +0100

    init

--- src/app/pages/add-reservation/add-reservation.component.ts
+++ src/app/pages/add-reservation/add-reservation.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../Components/navbar/navbar.component';
 import { RouterLink } from '@angular/router';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-add-reservation',
   standalone: true,
-  imports: [NavbarComponent,RouterLink],
+  imports: [NavbarComponent,RouterLink,MatFormFieldModule, MatDatepickerModule],
   templateUrl: './add-reservation.component.html',
+  providers: [provideNativeDateAdapter()],
   styleUrl: './add-reservation.component.scss'
 })
 export class AddReservationComponent {
